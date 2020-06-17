464049f089e687c074859307f2a905c674e2a83f
Tim Wells <invigoro@gmail.com>
2020-06-16 19:15:32 -0600

    plane controller

--- Assets/Scripts/PlanesScript.cs
+++ Assets/Scripts/PlanesScript.cs
@@ -6,7 +6,11 @@ public class PlanesScript : MonoBehaviour
 {
     //public parameters
 
-    public float moveSpeed = 1.0f;
+    public float maxSpeed = 100.0f;
+    public float currentSpeed = 10.0f;
+    public float airDrag = 1.0f;
+    public float acceleration = 1.0f;
+    public float liftFactor = 0.1f;
 
     //private vars
 
@@ -24,12 +28,24 @@ public class PlanesScript : MonoBehaviour
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
         var power = Input.GetAxis("Fire1");
-        var jump = Input.GetAxis("Jump");
+        var engine = Input.GetAxis("Thrust"); //fire the engine
 
 
-        var position = transform.forward * Time.deltaTime * moveSpeed * vertical;
-        position += transform.position;
-        rigidbody.MovePosition(position);
+        if(!Mathf.Approximately(engine, 0)) {
+            currentSpeed += acceleration;
+            currentSpeed = currentSpeed > maxSpeed ? maxSpeed : currentSpeed;
+        }
+        else
+        {
+            currentSpeed -= airDrag;
+            currentSpeed = currentSpeed < 0 ? 0f : currentSpeed;
+        }
+
+        rigidbody.AddRelativeForce(0, liftFactor * currentSpeed, currentSpeed);
+
+        //var position = transform.forward * Time.deltaTime * moveSpeed * vertical;
+        //position += transform.position;
+        //rigidbody.MovePosition(position);
 
     }
 }
