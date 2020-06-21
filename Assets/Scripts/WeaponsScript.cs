9dbe9593a7203dbeca95c536f271e83698a5819a
Tim Wells <invigoro@gmail.com>
2020-06-21 14:59:27 -0600

    Weapon stats

---
+++ Assets/Scripts/WeaponsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsScript : MonoBehaviour
+{
+    public float damage = 5f;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
