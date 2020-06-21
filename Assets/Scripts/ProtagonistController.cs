fed6f5367ec5b9cfcd739346558dff93972d8020
Tim Wells <invigoro@gmail.com>
2020-06-21 13:19:54 -0600

    Controller script

--- Assets/Scripts/ProtagonistController.cs
+++ Assets/Scripts/ProtagonistController.cs
@@ -11,6 +11,10 @@ public class ProtagonistController : MonoBehaviour
     public float walkBackwardsSpeed = 1.5f;
     public float sprintSpeed = 5.0f;
 
+    public float jumpTimeMultiplier = 10f;
+
+    public float blockingSpeedMultiplier = 0.75f;
+
 
     //private vars
     Animator animator;
@@ -24,12 +28,12 @@ public class ProtagonistController : MonoBehaviour
     bool run = false;
     bool sprint = false;
 
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
@@ -42,6 +46,7 @@ public class ProtagonistController : MonoBehaviour
         var walk = Input.GetAxis("Walk");
         var run = Input.GetAxis("Run");
         var sprint = Input.GetAxis("Sprint");
+        var block = !Mathf.Approximately(Input.GetAxis("Block"), 0);
 
 
         //set walk, run or sprint speed if input
@@ -67,10 +72,11 @@ public class ProtagonistController : MonoBehaviour
         animator.SetBool("run", this.run);
         animator.SetBool("sprint", this.sprint);
 
+        animator.SetBool("blockPressed", block);
         
 
 
-        if (isJumping == false && isLanding == false)
+        if (isJumping == false)
         {
 
             if (Mathf.Approximately(vertical, 0.0f)) //character is idling
@@ -80,28 +86,28 @@ public class ProtagonistController : MonoBehaviour
                 animator.SetBool("backwardPressed", false);
                 if (!Mathf.Approximately(jump, 0))
                 {
-                    startJump("idleIsSource", 0f, moveSpeed / 1.5f);
+                    startJump("idleIsSource", 0f, moveSpeed);
 
                 }
             }
             else if (vertical > 0) //character moving forward
             {
                 animator.SetBool("forwardPressed", true);
-                if (this.sprint)
+                if (this.sprint && !block)
                 {
                     moveSpeed = sprintSpeed;
                     if (!Mathf.Approximately(jump, 0))
                     {
-                        startJump("sprintIsSource", sprintSpeed, sprintSpeed / 2);
+                        startJump("sprintIsSource", sprintSpeed, sprintSpeed);
 
                     }
                 }
-                else if(this.run)
+                else if(this.run && !block)
                 {
                     moveSpeed = runSpeed;
                     if (!Mathf.Approximately(jump, 0))
                     {
-                        startJump("runIsSource", runSpeed, runSpeed / 2);
+                        startJump("runIsSource", runSpeed, runSpeed);
 
                     }
                 }
@@ -110,7 +116,7 @@ public class ProtagonistController : MonoBehaviour
                     moveSpeed = walkSpeed;
                     if (!Mathf.Approximately(jump, 0))
                     {
-                        startJump("walkIsSource", walkSpeed, walkSpeed / 2);
+                        startJump("walkIsSource", walkSpeed, walkSpeed);
 
                     }
                 }
@@ -121,7 +127,7 @@ public class ProtagonistController : MonoBehaviour
                 animator.SetBool("backwardPressed", true);
                 if (!Mathf.Approximately(jump, 0))
                 {
-                    startJump("backwardsIsSource", moveSpeed, moveSpeed / 2);
+                    startJump("backwardsIsSource", moveSpeed, moveSpeed);
 
                 }
             }
@@ -129,42 +135,49 @@ public class ProtagonistController : MonoBehaviour
 
             //animator.SetTrigger("endJump");
             var position = transform.forward * Time.deltaTime * moveSpeed * vertical;
+            if(block) { position *= blockingSpeedMultiplier; }
             position += transform.position;
             rigidBody.MovePosition(position);
         }
         else if (isJumping == true)
         {
             RaycastHit hit;
-            Debug.Log(rigidBody.velocity.y);
-            if (rigidBody.velocity.y < 0 && Physics.Raycast(rigidBody.position, rigidBody.velocity.normalized, out hit, (rigidBody.velocity.y * Time.deltaTime) + Mathf.Sqrt(-rigidBody.velocity.y)))
+            //Debug.Log($"y velocity : {rigidBody.velocity.y}");
+            var maxDist = Mathf.Abs(rigidBody.velocity.y) < 1 ? 1 : Mathf.Abs(DistanceTraveledY(rigidBody)) * jumpTimeMultiplier;
+            if (rigidBody.velocity.y < 0 && Physics.Raycast(rigidBody.position, new Vector3(0, -1, 0), out hit, maxDist))
             {
-                Debug.Log("Begin Landing");
-                isJumping = false;
-                isLanding = true;
-                animator.SetTrigger("endJump");
-                animator.SetBool("jumpPressed", false);
+                Debug.Log("Hit");
+                endJump();
             }
             else
             {
                 //Debug.Log("Miss");
             }
         }
-        else if (isLanding == true)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(rigidBody.position, -transform.up.normalized, out hit, 0.2f))
-            {
-                Debug.Log("Hit Ground");
-                animator.SetTrigger("Land");
-                isLanding = false;
-            }
-            else
-            {
-                //Debug.Log("Miss");
-            }
-        }
+        //else if (isLanding == true)
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(rigidBody.position, -transform.up.normalized, out hit, 0.2f))
+        //    {
+        //        Debug.Log("Hit Ground");
+        //        animator.SetTrigger("Land");
+        //        isLanding = false;
+        //    }
+        //    else
+        //    {
+        //        //Debug.Log("Miss");
+        //    }
+        //}
         Vector3 rotate = new Vector3(0, horizontal * turnSpeed * Time.deltaTime / Mathf.Sqrt(moveSpeed), 0);
         transform.Rotate(rotate);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(isJumping && collision.gameObject.GetComponentInParent<GroundScript>() != null)
+        {
+            endJump();
+        }
     }
     //void OnCollisionEnter(Collision other)
     //{
@@ -193,5 +206,20 @@ public class ProtagonistController : MonoBehaviour
             //Debug.Log(force);
             rigidBody.AddForce(force, ForceMode.VelocityChange);
         }
+    }
+
+    public void endJump()
+    {
+        Debug.Log("Begin Landing");
+        isJumping = false;
+        animator.SetTrigger("endJump");
+        animator.SetBool("jumpPressed", false);
+    }
+
+    public float DistanceTraveledY(Rigidbody rigidBody)
+    {
+        var p1 = (Physics.gravity.y * Mathf.Pow(Time.deltaTime, 2)) + (rigidBody.velocity.y * Time.deltaTime);
+        Debug.Log($"Distance called: {p1}");
+        return p1;
     }
 }
